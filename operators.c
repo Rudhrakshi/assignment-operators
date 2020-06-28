2d5bf0e2570d7223e2bf49c263cf0f55f48e7e04
Rudhrakshi <67533685+Rudhrakshi@users.noreply.github.com>
2020-06-29 00:40:13 +0530

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,22 @@
+//ASSIGNMENT:OPERATORS 28 JUNE 2020  UNSCHOOL
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+    printf("enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    int c=a+b;
+    int d=a-b;
+    int e=a*b;
+    int f=a/b;
+    int g=a%b;
+    printf("addition of two no. is %d\n",c);
+    printf("subtraction of two no. is %d\n",d);
+    printf("multiplication of two no. is %d\n",e);
+    printf("division of two no. is %d\n",f);
+    printf("modulus of two no. is %d\n",g);
+    return 0;
+
+}
+
